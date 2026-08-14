aa37127d390b21d691d8d646c74ec32af653e5e9
AbbosMamadaliyev <abbbos166@gmail.com>
2026-08-14 08:35:06 +0500

    Initial Commit

---
+++ DuoCam/DuoCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuoCamApp.swift
+//  DuoCam
+//
+//  Created by user on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct DuoCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
